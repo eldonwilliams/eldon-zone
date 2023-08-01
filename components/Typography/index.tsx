c5d6b54f21ab2166ffeb8b6463203bb1f2ca7016
Eldon Williams <eldon.ew.com@gmail.com>
2023-08-01 18:16:52 -0500

    :sparkles: add support for twemoji

--- components/Typography/index.tsx
+++ components/Typography/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { TypographyProps } from "./TypographyProps";
 import TypographyVariant from "./TypographyVariant";
+import emoji from "react-easy-emoji";
+
 
 /**
  * Typography - A component to display text
@@ -9,12 +11,17 @@ export default function Typography(props: TypographyProps) {
 	const { children, twemoji, color, interactive, bold, italic, underline, highlight, noselect, ...componentProps } = props;
 	const styleProps = { color, interactive, bold, italic, underline, highlight, noselect };
 	const isTextTypography = typeof children === "string";
-	
+
 	return (
 		React.createElement(
 			isTextTypography ? "p" : "span", // If the children is a string, use a paragraph tag, otherwise use a span tag
 			{ ...componentProps, className: `${componentProps.className ?? ""} ${TypographyVariant(styleProps)}`, }, // Spread the component props and add the TypographyVariant class
-			children
+			(twemoji ?? true) && isTextTypography ?
+				emoji(children, {
+					ext: '.png',
+					props: { className: "inline", }
+				}) :
+				children // If twemoji is true, or undefined, and the children is a string, wrap in Twemoji
 		)
 	);
 }
