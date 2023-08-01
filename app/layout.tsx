31077e1f994e1ee5fe72d2561002399f9113214d
Eldon Williams <eldon.ew.com@gmail.com>
2023-07-31 21:30:20 -0500

    :tada: init

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Eldon W',
+  description: 'Eldon Williams\'s personal website',
+  authors: {name: 'Eldon Williams', url: 'https://eldon.zone/'},
+  robots: "index, follow",
+  themeColor: "#000000",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
