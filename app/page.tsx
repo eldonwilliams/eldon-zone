37619b8f2b037dfc293bdafb683f571e5766a6e5
Eldon Williams <eldon.ew.com@gmail.com>
2024-02-05 14:02:54 -0600

    add informational about update

--- app/page.tsx
+++ app/page.tsx
@@ -9,24 +9,34 @@ import Bullet from "./components/Bullet";
 export default function Home() {
 	return (
 		<>
-			<header className="max-w-max m-3">
-				<Typography block element="h1" font="mono" weight="black" className="text-4xl -mb-1">Hello,&nbsp;World!</Typography>
-				<Typography block element="h1" font="mono" weight="thin" className="text-2xl">
-					<Bullet />
-					<Typography>Eldon Williams</Typography>
-					<Typography color="info">;</Typography>
+			<div className="text-center p-2 px-4 rounded-full border dark:border-stone-500 dark:shadow-none dark:drop-shadow-none shadow-sm drop-shadow-sm m-2 w-max">
+				<Typography className="hidden sm:block">
+					✨ Hello Visitor! An update is coming later this year. Please check back to see it!
 				</Typography>
-			</header>
-			<main className="m-2 sm:mx-6 sm:flex sm:flex-row sm:flex-wrap sm:gap-4 sm:items-start sm:justify-center">
-				<Contact />
-				<About />
-				<Languages />
-				<Skills />
-				<Improving />
-			</main>
-			<footer className="p-2 w-full sm:w-auto">
-				<Typography className="text-center min-w-full" color="secondary" element="p">Made with ❤️ by Eldon Williams</Typography>
-			</footer>
+				<Typography className="block sm:hidden">
+					✨ Hello Visitor! An update is coming later this year.
+				</Typography>
+			</div>
+			<div>
+				<header className="max-w-max m-3">
+					<Typography block element="h1" font="mono" weight="black" className="text-4xl -mb-1">Hello,&nbsp;World!</Typography>
+					<Typography block element="h1" font="mono" weight="thin" className="text-2xl">
+						<Bullet />
+						<Typography>Eldon Williams</Typography>
+						<Typography color="info">;</Typography>
+					</Typography>
+				</header>
+				<main className="m-2 sm:mx-6 sm:flex sm:flex-row sm:flex-wrap sm:gap-4 sm:items-start sm:justify-center">
+					<Contact />
+					<About />
+					<Languages />
+					<Skills />
+					<Improving />
+				</main>
+				<footer className="p-2 w-full sm:w-auto">
+					<Typography className="text-center min-w-full" color="secondary" element="p">Made with ❤️ by Eldon Williams</Typography>
+				</footer>
+			</div>
 		</>
 	)
 }
